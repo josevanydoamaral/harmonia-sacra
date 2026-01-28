2f4104efe309677e6bad9d49a88b8d94d02755b0
Josevany do Amaral <97293369+josevanydoamaral@users.noreply.github.com>
2026-01-28 12:39:04 +0000

    chore: setup inicial do projeto com vite, typescript e tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

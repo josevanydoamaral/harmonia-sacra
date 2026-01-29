05b0f409d9a2841ce412bfe936c91750a6c1c8ea
Josevany do Amaral <97293369+josevanydoamaral@users.noreply.github.com>
2026-01-29 17:42:25 +0000

    feat: Integra firestore em tempo real e unifica lógica de filtro na home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center p-6 bg-white/93'>
+    <header className='flex justify-between items-center p-6 bg-white/93 shadow-2xl'>
         <h1 className='font-serif text-2xl text-[#5c4d37]'>Harmonia Sacra</h1>
         <button className='bg-[#b38b4d] text-white rounded-2xl px-6 py-2 shadow-md cursor-pointer'>&hearts; Doe</button>
     </header>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,56 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
 import backgroundImage from '../assets/hero-img.png'
 import Header from './Header'
 import SearchBar from './SearchBar'
 import SongCard from './SongCard'
 import { motion } from 'framer-motion'
+import { db } from '../lib/firebase'
 
 const Voices = {
-    oneVoice: 1,
-    twoVoices: 2,
-    threeVoices: 3,
-    fourVoices: 4
+  oneVoice: 1,
+  twoVoices: 2,
+  threeVoices: 3,
+  fourVoices: 4
 } as const
 
 type VoicesType = typeof Voices[keyof typeof Voices]
 
-const songs = [
-  { id: 1, title: "Ave Maria", composer: "Victoria", category: "Advento", voices: 4 },
-  { id: 2, title: "Kyrie Eleison", composer: "Palestrina", category: "Comum", voices: 4 },
-  { id: 3, title: "O Magnum Mysterium", composer: "Victoria", category: "Natal", voices: 4 },
-    { id: 4, title: "Ave Maria", composer: "Victoria", category: "Advento", voices: 4 },
-  { id: 5, title: "Kyrie Eleison", composer: "Palestrina", category: "Comum", voices: 4 },
-  { id: 6, title: "O Magnum Mysterium", composer: "Victoria", category: "Natal", voices: 4 },
-    { id: 7, title: "Ave Maria", composer: "Victoria", category: "Advento", voices: 4 },
-  { id: 8, title: "Kyrie Eleison", composer: "Palestrina", category: "Comum", voices: 4 },
-  { id: 9, title: "O Magnum Mysterium", composer: "Victoria", category: "Natal", voices: 4 },
-    { id: 10, title: "Ave Maria", composer: "Victoria", category: "Advento", voices: 4 },
-  { id: 11, title: "Kyrie Eleison", composer: "Palestrina", category: "Comum", voices: 4 },
-  { id: 12, title: "O Magnum Mysterium", composer: "Victoria", category: "Natal", voices: 4 },
-];
-
 const Home = () => {
   const [search, setSearch] = useState("")
+  const [songs, setSongs] = useState<any[]>([])
+
+  useEffect(() => {
+    const songsCollection = collection(db, "songs");
+
+    const unsubscribe = onSnapshot(songsCollection,
+      (snapshot) => {
+    
+        const songsData = snapshot.docs.map(
+          doc => ({ id: doc.id, ...doc.data() }))
+        setSongs(songsData)
+      },
+      (error) => { console.error("Erro no Firebase: ", error) })
+
+    return () => unsubscribe();
+  }, [])
+
   const filteredSongs = songs.filter(fs => fs.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
   return (
-      <div className='relative min-h-screen bg-[#fcf2d9]'>
+    <div className='relative min-h-screen bg-[#f6e1ab]'>
 
-        <div style={{ backgroundImage: `url(${backgroundImage})` }} className='absolute top-0 left-0 w-full h-100 bg-no-repeat bg-fixed bg-center bg-cover opacity-50'>
-        </div>
-
-        <div className='absolute w-full h-100 bg-linear-to-b from-transparent to-[#fcf2d9]'></div>
-
-        <div className='relative z-10'>
-          <Header />
-
-          <SearchBar value={search} onChange={setSearch}/>
-
-          <motion.div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-30 px-3 items-start transition-all duration-1000">
-            {filteredSongs.map(song => <SongCard key={song.id} id={song.id} title={song.title} composer={song.composer} category={song.category} voices={song.voices as VoicesType} />)}
-          </motion.div>
-          
-        </div>
+      <div style={{ backgroundImage: `url(${backgroundImage})` }} className='absolute top-0 left-0 w-full h-100 bg-no-repeat bg-fixed bg-center bg-cover opacity-50'>
       </div>
+
+      <div className='absolute w-full h-100 bg-linear-to-b from-transparent to-[#f6e1ab]'></div>
+
+      <div className='relative z-10'>
+        <Header />
+
+        <SearchBar value={search} onChange={setSearch} />
+
+        <motion.div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-30 px-6 items-start transition-all duration-1000">
+          {filteredSongs.map(song => <SongCard key={song.id} id={song.id} title={song.title} composer={song.composer} category={song.category} voices={song.voices as VoicesType} />)}
+        </motion.div>
+
+      </div>
+    </div>
   )
 }
 

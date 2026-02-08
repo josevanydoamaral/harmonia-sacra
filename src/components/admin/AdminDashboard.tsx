21067649a33717192e466502297f2d6d6dbed82b
Josevany do Amaral <97293369+josevanydoamaral@users.noreply.github.com>
2026-02-08 17:05:30 +0000

    style: estilização básica da página de admin

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { type Song, type VoicesType } from '../../types/song'
 import AdminSongRow from './AdminSongRow';
 import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
+import { Plus } from 'lucide-react';
 
 
 
 const AdminDashboard = () => {
-    const [songs, setSongs] = useState<Song[]>([])
+    const navigate = useNavigate();
+    const [songs, setSongs] = useState<Song[]>([]);
 
     async function handleEdit(id: string): Promise<void> {
         console.log("Oi")
@@ -41,8 +44,8 @@ const AdminDashboard = () => {
     return (
         <div className='max-w-6xl mx-auto p-8'>
             <div className='flex justify-between'>
-                <h3>Admin Dashboard</h3>
-                <button>OI</button>
+                <h3 className='text-2xl font-serif text-[#B38B4D] mb-8'>Admin Dashboard</h3>
+                <button className='flex items-center gap-2 bg-[#a07a3d] text-white px-5 py-2 rounded-lg font-medium transition-all hover:scale-105 active:scale-95 shadow-lg' ><Plus /></button>
             </div>
 
             <div className='grid grid-cols-[1fr_150px_120px_100px] items-center px-4 py-2 bg-track-button/10 text-track-text text-xs font-bold uppercase tracking-wider rounded-t-lg'>

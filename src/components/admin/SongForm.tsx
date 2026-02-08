103d9128dad90ce8ab927e0cab3c7d7f003a3337
Josevany do Amaral <97293369+josevanydoamaral@users.noreply.github.com>
2026-02-08 18:20:48 +0000

    fix: Corrigi a estrutura do layout e mapeei a categoria e a voz

--- src/components/admin/SongForm.tsx
+++ src/components/admin/SongForm.tsx
@@ -33,39 +33,45 @@ const SongForm = () => {
                     <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>Compositor</label>
 
                     <input className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' type="text" placeholder='Ex: A. Cartageno' />
-                    <div className='flex gap-81'>
-                        <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>Categoria</label>
-                        <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>Vozes</label>
-
-                    </div>
 
                     <div className='flex gap-4'>
-                        
-                        <select className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' name="" id="">
-                            <option value="volvo">Volvo</option>
-                        </select>
+                        <div className="flex flex-col w-full">
 
-                        
-                        <select className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' name="" id="">
-                            <option value="volvo">1</option>
-                        </select>
+                            <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>Categoria</label>
+
+                            <select className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' name="" id="">{['advento', 'natal', 'comum', 'quaresma', 'pascoa', 'pentecostes'].map((categoria) => (
+                                <option key={categoria}>{categoria.toLocaleUpperCase()}</option>
+                            ))}</select>
+
+                        </div>
+
+                        <div className="flex-col w-1/3">
+                            <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>Vozes</label>
+
+                            <select className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' name="" id="">
+                                {['1', '2', '3', '4'].map((voice) => (
+                                    <option value={voice} key={voice}>{voice}</option>
+                                ))}
+                            </select>
+                        </div>
+
                     </div>
                 </div>
                 <div className="space-y-1">
                     {['soprano', 'contralto', 'tenor', 'bass'].map((voice) => (
                         <div key={voice}>
                             <label className='block text-xs font-bold uppercase tracking-wider text-track-text/60 mb-2'>{voice}</label>
-                            <input 
-                                className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' type="text" 
-                                placeholder={`URL do áudio ${voice}`} 
+                            <input
+                                className='w-full bg-base-surface border border-track-border rounded-md px-4 py-2 focus:border-accent-gold outline-none transition-all' type="text"
+                                placeholder={`URL do áudio ${voice}`}
                                 value={(formData.audioUrls as any)[voice]}
                                 onChange={(e) => handleAudioChange(voice, e.target.value)}
-                                />
+                            />
                         </div>
                     ))}
                 </div>
             </div>
-            <div className='flex justify-end gap-6'>
+            <div className='flex justify-end gap-6 mt-8'>
                 <button className='text-track-text/50 hover:text-track-text transition-colors'>Cancelar</button>
                 <button className='bg-[#B38B4D] hover:bg-[#a07a3d] text-white px-8 py-3 rounded-lg font-bold shadow-lg transition-all hover:scale-105' type='submit'>Guardar</button>
             </div>

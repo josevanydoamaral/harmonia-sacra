d594482761f54839b223f054c88ad85300b24383
josevanydoamaral <josevanydoamaral@gmail.com>
2026-04-22 14:37:36 +0200

    fix(firebase):: ensure environment variablees and config are ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import SongDetail from './components/SongDetail'
 import Layout from './components/Layout'
@@ -7,8 +7,17 @@ import AdminDashboard from './components/admin/AdminDashboard'
 import SongForm from './components/admin/SongForm'
 
 function App() {
+  const routes = createRoutesFromElements(
+    <>
+      <Route index path='/' element={<Home />} />
+      <Route index path='/cantico/:id' element={<SongDetail />} />
+    </>
+  )
+  
+  const router = createBrowserRouter(routes);
+
   return (
-    <SongForm />
+    <RouterProvider router={router} />
   )
 }
 

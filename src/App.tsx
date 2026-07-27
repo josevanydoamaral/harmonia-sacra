e05671557bb1667544c345be15258a505bc4338c
Josevany Amaral <josevany.amaral@conkord.com>
2026-07-27 14:07:37 +0100

    feat: add ProtectedRoute component and protect dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,21 @@ import AdminSongRow from './components/admin/AdminSongRow'
 import AdminDashboard from './components/admin/AdminDashboard'
 import SongForm from './components/admin/SongForm'
 import Login from './pages/login'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import Dashboard from './components/admin/Dashboard'
 
 
 function App() {
   const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index path='/' element={<Home />} />
+      <Route index element={<Home />} />
       <Route path='/cantico/:id' element={<SongDetail />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={
+        <ProtectedRoute>
+            <Dashboard />
+        </ProtectedRoute>
+      }/>
     </Route>
   )
   
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import type React from "react";
+import { createContext, useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    const { user, loading } = useAuth();
+
+
+    if (loading) {
+        return <div className="min-h-screen flex items-center justify-center text-text-main">A carregar...</div>
+    }
+
+    if (!user) {
+        return <Navigate replace to="/login" />
+    }
+
+    return children;
+
+}

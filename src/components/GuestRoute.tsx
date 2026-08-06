c1f189882f4c134c51fd4c65b0f8b115c953f8e6
Josevany Amaral <josevany.amaral@conkord.com>
2026-08-06 16:39:07 +0100

    fix: exclude useNavigate import

--- src/components/GuestRoute.tsx
+++ src/components/GuestRoute.tsx
@@ -1,10 +1,9 @@
 import type { ReactNode } from "react";
 import { useAuth } from "../context/AuthContext";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const GuestRoute = ({ children }: { children: ReactNode}) => {
     const { user, loading } = useAuth();
-    const navigate = useNavigate();
 
     if (loading) {
         return <div className="min-h-screen flex items-center justify-center text-text-main">A carregar...</div>

7cc50dacc60f0d71d35690784f6c95a6f1a6ccfd
Josevany Amaral <josevany.amaral@conkord.com>
2026-08-12 11:58:19 +0100

    recover ToastContext code lost in stash

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -16,6 +16,8 @@ export interface ToastContextType {
     showInfo(message: string): void;
 
     removeToast(id: string): void;
+
+    toasts: Toast[];
 }
 
 export const ToastContext = createContext<ToastContextType | undefined>(undefined);
@@ -24,6 +26,38 @@ export const ToastProvider = ({ children }: { children: ReactNode }) => {
     const [toasts, setToasts] = useState<Toast[]>([])
     
     function removeToast(id: string) {
-        
+        setToasts((prev) => prev.filter((t) => t.id !== id));
     }
+
+    function addToast(message: string, type: ToastType) {
+        const id = crypto.randomUUID();
+
+        const newToast: Toast = { id, message, type };
+
+        setToasts((prev) => [...prev, newToast]);
+
+        setTimeout(() => {
+            removeToast(id);
+        }, 4000)
+    }
+
+    function showError(message: string) {
+        addToast(message, 'error');
+    }
+
+    function showSuccess(message: string) {
+        addToast(message, 'success');
+    }
+
+    function showInfo(message:string) {
+        addToast(message, 'info')
+    }
+
+    return (
+        <ToastContext.Provider value={{ toasts, showError, showSuccess, showInfo, removeToast }}>
+            {children}
+        </ToastContext.Provider>
+
+    )
+
 }
